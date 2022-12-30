c41282dc443c75f193eee96649eada5b5b1c05ab
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2022-12-30 01:00:36 +0100

    Update ft_ftoa.c

--- with_flags/sources/ft_ftoa.c
+++ with_flags/sources/ft_ftoa.c
@@ -6,64 +6,11 @@
 /*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 16:45:47 by mgama             #+#    #+#             */
-/*   Updated: 2022/12/30 00:58:02 by mgama            ###   ########.fr       */
+/*   Updated: 2022/12/30 01:00:34 by mgama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
-
-// char	*ft_ftoa(double x, int max)
-// {
-// 	int		n;
-// 	int		i;
-// 	int		k;
-// 	char	*p;
-// 	char	*tmp;
-// 	i = 0;
-// 	k = 0;
-// 	p = ft_calloc(20, sizeof(char));
-// 	if (!p)
-// 		return (0);
-// 	if (x < 0)
-// 	{
-// 		x = -x;
-// 		p[i] = '-';
-// 		i++;
-// 		k++;
-// 	}
-// 	n = (int)x;
-// 	tmp = ft_itoa(n);
-// 	ft_strlcat(p, tmp, 20);
-// 	free(tmp);
-// 	if (n == 0)
-// 	{
-// 		k++;
-// 		i++;
-// 	}
-// 	while (n > 0)
-// 	{
-// 		x /= 10;
-// 		n = (int)x;
-// 		i++;
-// 	}
-// 	*(p + i) = '.';
-//  	x *= 10;
-//  	n = (int)x;
-//  	x = x - n;
-//  	while (((n > 0) || (i > k)) && max > 0)
-//  	{
-//  		if (k == i)
-//  			k++;
-// 		*(p + k) = '0' + n;
-// 		x *= 10;
-// 		n = (int)x;
-// 		x = x - n;
-// 		k++;
-// 		max--;
-// 	}
-// 	*(p + k) = '\0';
-// 	return (p);
-// }
 
 char	*ft_ftoa(double n, int afterpoint)
 {
