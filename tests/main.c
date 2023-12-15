465fc5a33b152b1320649014dd3f1bd43457584e
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2023-12-15 01:13:00 +0100

    Update main.c

--- tests/main.c
+++ tests/main.c
@@ -4,48 +4,48 @@
 int main(void)
 {
 	printf("len %d\n", printf("%.30f\n", 4242.28730494752873049475));
-	// printf("len %d\n", ft_printf("%.30f\n", 4242.28730494752873049475));
+	printf("len %d\n", ft_printf("%.30f\n", 4242.28730494752873049475));
 	printf("len %d\n", printf("%f\n", 4242.28));
-	// printf("len %d\n", ft_printf("%f\n", 4242.28));
+	printf("len %d\n", ft_printf("%f\n", 4242.28));
 	
 	printf("len %d\n", printf("%f\n", 4242.2842));
-	// printf("len %d\n", ft_printf("%f\n", 4242.2842));
+	printf("len %d\n", ft_printf("%f\n", 4242.2842));
 	printf("len %d\n", printf("%f\n", 4242.284201));
-	// printf("len %d\n", ft_printf("%f\n", 4242.284201));
+	printf("len %d\n", ft_printf("%f\n", 4242.284201));
 
 	printf("len %d\n", printf("%f\n", 4242.101));
-	// printf("len %d\n", ft_printf("%f\n", 4242.101));
+	printf("len %d\n", ft_printf("%f\n", 4242.101));
 
 	printf("len %d\n", printf("%f\n", 4242.01));
-	// printf("len %d\n", ft_printf("%f\n", 4242.01));
+	printf("len %d\n", ft_printf("%f\n", 4242.01));
 	printf("len %d\n", printf("%f\n", 4242.001));
-	// printf("len %d\n", ft_printf("%f\n", 4242.001));
+	printf("len %d\n", ft_printf("%f\n", 4242.001));
 
 	printf("len %d\n", printf("%f\n", 4242.000000001));
-	// printf("len %d\n", ft_printf("%f\n", 4242.000000001));
+	printf("len %d\n", ft_printf("%f\n", 4242.000000001));
 
 	printf("len %d\n", printf("%-x\n", 4242));
-	// printf("len %d\n", ft_printf("%-x\n", 4242));
+	printf("len %d\n", ft_printf("%-x\n", 4242));
 	printf("len %d\n", printf("%010x\n", 4242));
-	// printf("len %d\n", ft_printf("%010x\n", 4242));
+	printf("len %d\n", ft_printf("%010x\n", 4242));
 	printf("len %d\n", printf("%-10x\n", 4242));
-	// printf("len %d\n", ft_printf("%-10x\n", 4242));
+	printf("len %d\n", ft_printf("%-10x\n", 4242));
 	printf("len %d\n", printf("%10d\n", 42));
-	// printf("len %d\n", ft_printf("%10d\n", 42));
+	printf("len %d\n", ft_printf("%10d\n", 42));
 
 	printf("len %d\n", printf("%.4s\n", "hello"));
-	// printf("len %d\n", ft_printf("%.4s\n", "hello"));
+	printf("len %d\n", ft_printf("%.4s\n", "hello"));
 	printf("len %d\n", printf("%.4s\n", NULL));
-	// printf("len %d\n", ft_printf("%.4s\n", NULL));
+	printf("len %d\n", ft_printf("%.4s\n", NULL));
 	
 	printf("len %d\n", printf("%#.10X\n", 0));
-	// printf("len %d\n", ft_printf("%#.10X\n", 0));
+	printf("len %d\n", ft_printf("%#.10X\n", 0));
 	printf("len %d\n", printf("%#.10X\n", 42));
-	// printf("len %d\n", ft_printf("%#.10X\n", 42));
+	printf("len %d\n", ft_printf("%#.10X\n", 42));
 
 	printf("len %d\n", printf("%*x\n", 5, 42));
-	// printf("len %d\n", ft_printf("%*x\n", 5, 42));
+	printf("len %d\n", ft_printf("%*x\n", 5, 42));
 	printf("len %d\n", printf("%.*s\n", 4, "hello"));
-	// printf("len %d\n", ft_printf("%.*s\n", 4, "hello"));
+	printf("len %d\n", ft_printf("%.*s\n", 4, "hello"));
 	return (0);
 }
