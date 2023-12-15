7aa55487acdf8a23a622ed739c509e6ea2f398fe
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2023-12-15 19:34:40 +0100

    Update ft_print_int_bonus.c

--- bonus/sources/ft_print_int_bonus.c
+++ bonus/sources/ft_print_int_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 20:17:43 by mgama             #+#    #+#             */
-/*   Updated: 2023/12/15 19:30:58 by mgama            ###   ########.fr       */
+/*   Updated: 2023/12/14 23:52:09 by mgama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,35 +16,27 @@
 static void	ft_print_in_int_spaces(char *d_i, int save_i,
 	int *count, t_flags flags)
 {
-	size_t	len;
-
-	len = ft_strlen(d_i);
-	if (save_i < 0 && flags.dot >= 0)
+	if (save_i < 0 && flags.plus == 0 && flags.dot >= 0)
 		ft_putchar_c('-', count);
 	if (flags.dot >= 0)
-		ft_print_width(flags.dot - 1, len - 1, 1, count);
-	if (!(flags.dot == 0 && save_i == 0))
-		ft_putstrprec(d_i, len, count);
+		ft_print_width(flags.dot - 1, ft_strlen(d_i) - 1, 1, count);
+	ft_putstrprec(d_i, ft_strlen(d_i), count);
 }
 
 static void	ft_print_int_spaces(char *d_i, int save_i,
 	int *count, t_flags flags)
 {
-	size_t	len;
-
-	len = ft_strlen(d_i);
 	if (flags.minus == 1)
 		ft_print_in_int_spaces(d_i, save_i, count, flags);
-	if (flags.dot >= 0 && (size_t)flags.dot < len && save_i > 0)
-		flags.dot = len;
+	if (flags.dot >= 0 && (size_t)flags.dot < ft_strlen(d_i))
+		flags.dot = ft_strlen(d_i);
 	if (flags.dot >= 0)
-		ft_print_width(flags.width, len, 0, count);
+	{
+		flags.width -= flags.dot;
+		ft_print_width(flags.width, 0, 0, count);
+	}
 	else
-		ft_print_width(flags.width, len, flags.zero, count);
-	if (save_i >= 0 && flags.plus == 1)
-		ft_putstrprec("+", 1, count);
-	else if (save_i >= 0 && flags.blank == 1)
-		ft_putstrprec(" ", 1, count);
+		ft_print_width(flags.width, ft_strlen(d_i), flags.zero, count);
 	if (flags.minus == 0)
 		ft_print_in_int_spaces(d_i, save_i, count, flags);
 }
@@ -61,10 +53,12 @@ static void	ft_print_int_flags(t_flags *flags, long int *li, int *count)
 	}
 	else if (*li >= 0 && flags->plus == 1)
 	{
+		ft_putstrprec("+", 1, count);
 		flags->width--;
 	}
 	else if (*li >= 0 && flags->blank == 1)
 	{
+		ft_putstrprec(" ", 1, count);
 		flags->width--;
 	}
 }
@@ -75,6 +69,11 @@ void	ft_print_int(int i, int *count, t_flags flags)
 	long int	li;
 
 	li = i;
+	if (flags.dot == 0 && li == 0)
+	{
+		ft_print_width(flags.width, 0, 0, count);
+		return ;
+	}
 	ft_print_int_flags(&flags, &li, count);
 	d_i = ft_itoa(li);
 	if (!d_i)
