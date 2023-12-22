48bee1b4dea19e0d17535a3df633a546ff767189
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2023-12-22 12:28:04 +0100

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 11:20:24 by mgama             #+#    #+#             */
-/*   Updated: 2023/12/22 11:22:06 by mgama            ###   ########.fr       */
+/*   Updated: 2023/12/22 12:27:33 by mgama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
  * 
  */
 
-#ifndef FT_PRINTF_42_H
-#define FT_PRINTF_42_H
+#ifndef FT_PRINTF
+#define FT_PRINTF
 
 int		ft_printf(const char *fmt, ...);
 
-#endif /* FT_PRINTF_42_H */
+#endif /* FT_PRINTF */
