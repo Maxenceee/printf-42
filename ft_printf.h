927ed85155361ec3719e76d0db3180d84254a47f
Maxenceee <72473014+Maxenceee@users.noreply.github.com>
2023-12-22 11:22:24 +0100

    Create ft_printf.h

---
+++ ft_printf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgama <mgama@student.42lyon.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/22 11:20:24 by mgama             #+#    #+#             */
+/*   Updated: 2023/12/22 11:22:06 by mgama            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * 
+ * To include this lib please use this header file.
+ * 
+ */
+
+#ifndef FT_PRINTF_42_H
+#define FT_PRINTF_42_H
+
+int		ft_printf(const char *fmt, ...);
+
+#endif /* FT_PRINTF_42_H */
